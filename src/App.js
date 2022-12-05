7e4713f6086b4294ad5ed28330cb6524f02f3a00
codessi <johansuh@gmail.com>
2022-12-05 09:52:43 -0800

    change app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header/Header';
+import React from "react"
+import "./App.css"
+import Header from "./components/Header/Header"
 
 function App() {
   return (
-    <React.StrictMode>
+    <>
       {/* <NavMenu /> */}
       <Header />
       {/* <AboutMe /> */}
       {/* <Content /> */}
       {/* <Contact /> */}
       {/* <Footer /> */}
-    </React.StrictMode>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
